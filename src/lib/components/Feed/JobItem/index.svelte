d1b230bb9c3dc23acdae7255ccdab12938c34410
4kcancode <de4kcancode@outlook.com>
2023-05-19 10:45:30 +0300

    Add job feed item

---
+++ src/lib/components/Feed/JobItem/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import TextParagraph from '$components/TextParagraph/index.svelte';
+	import type { ParsedMessage } from '$components/BodyParser';
+
+	export let message: ParsedMessage;
+</script>
+<style>
+.disc{
+	width: 8px;
+	height: 8px;
+	background: #A2A7B8;
+	top: 10px;
+	left: -20px;
+}
+
+</style>
+<template lang="pug" src="./template.pug">
+
+</template>
