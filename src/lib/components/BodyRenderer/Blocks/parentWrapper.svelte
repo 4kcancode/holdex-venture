590c7e6c0721c2fa2f7dd780d0bdc3ec91b279b7
4kcancode <de4kcancode@outlook.com>
2023-05-24 14:54:51 +0530

    using svelte:element in parentWrapper

--- src/lib/components/BodyRenderer/Blocks/parentWrapper.svelte
+++ src/lib/components/BodyRenderer/Blocks/parentWrapper.svelte
@@ -2,10 +2,14 @@
 	import Link from './link.svelte';
 	export let item: any;
 	export let value: string;
+
+	function itemTypeToTagMapper(itemType:string): string {
+		return 'p'
+	}
 </script>
 
 {#if value === 'table'}
-	{#if item.href}
+	{#if item.href && item.type === 'link'}
 		<Link
 			{item}
 			classes={'relative inline-block underline underline-offset-4 bg-accent1-default/15 text-accent1-default transition-colors hover:bg-accent1-default/26'}
@@ -13,6 +17,8 @@
 			{item.text}
 		</Link>
 	{:else}
-		{item.text}
+		<svelte:element this={itemTypeToTagMapper(item.type)}>
+			{item.text}
+		</svelte:element>
 	{/if}
 {/if}
