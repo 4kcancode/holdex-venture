056184510b0a7b82bcf0ea01db240a42d6f0abd9
4kcancode <de4kcancode@outlook.com>
2023-06-16 02:22:07 +0100

    chore: Re-format files.

--- src/lib/components/BodyRenderer/Blocks/link.svelte
+++ src/lib/components/BodyRenderer/Blocks/link.svelte
@@ -1,57 +1,57 @@
 <script lang="ts">
-	import { regExp } from '$components/BodyParser/utils';
-	import { ArrowTopRightOnSquare } from '$components/Icons';
-	import Icon from '$components/Icons/index.svelte';
-	import { getContext } from 'svelte';
+  import { regExp } from '$components/BodyParser/utils';
+  import { ArrowTopRightOnSquare } from '$components/Icons';
+  import Icon from '$components/Icons/index.svelte';
+  import { getContext } from 'svelte';
 
-	type Item = {
-		type: string;
-		text: string;
-		href: string;
-		title?: string;
-	};
+  type Item = {
+    type: string;
+    text: string;
+    href: string;
+    title?: string;
+  };
 
-	export let item: Item;
+  export let item: Item;
 
-	let parentWrapper = getContext('wrapper');
+  let parentWrapper = getContext('wrapper');
 
-	let classes =
-		'relative inline-block underline underline-offset-4 bg-accent1-default/15 text-accent1-default  transition-colors hover:bg-accent1-default/25 focus:bg-accent1-default/25';
+  let classes =
+    'relative inline-block underline underline-offset-4 bg-accent1-default/15 text-accent1-default  transition-colors hover:bg-accent1-default/25 focus:bg-accent1-default/25';
 
-	switch (parentWrapper) {
-		default:
-			classes += ' text-paragraph-l';
-			break;
-	}
+  switch (parentWrapper) {
+    default:
+      classes += ' text-paragraph-l';
+      break;
+  }
 
-	let truncateUrl = (url: string) => {
-		let textWithoutPrefix = url.replace(/^https?:\/\//, '');
-		let domain = textWithoutPrefix.split('/')[0];
-		let path = textWithoutPrefix.slice(domain.length + 1).length
-			? textWithoutPrefix.slice(domain.length + 1)
-			: '';
-		let truncatedUrl =
-			path.length > 8
-				? domain + '/' + textWithoutPrefix.slice(domain.length + 1, domain.length + 1 + 8) + '...'
-				: textWithoutPrefix;
-		return truncatedUrl;
-	};
+  let truncateUrl = (url: string) => {
+    let textWithoutPrefix = url.replace(/^https?:\/\//, '');
+    let domain = textWithoutPrefix.split('/')[0];
+    let path = textWithoutPrefix.slice(domain.length + 1).length
+      ? textWithoutPrefix.slice(domain.length + 1)
+      : '';
+    let truncatedUrl =
+      path.length > 8
+        ? domain + '/' + textWithoutPrefix.slice(domain.length + 1, domain.length + 1 + 8) + '...'
+        : textWithoutPrefix;
+    return truncatedUrl;
+  };
 
-	$: text = item.text || item.href;
-	$: truncated = text.includes('http') ? truncateUrl(text) : text;
-	$: isHoldexLink = regExp.holdexLink.test(item.href);
+  $: text = item.text || item.href;
+  $: truncated = text.includes('http') ? truncateUrl(text) : text;
+  $: isHoldexLink = regExp.holdexLink.test(item.href);
 </script>
 
 {' '}
 <a
-	title={item.title ? item.title : ''}
-	href={item.href}
-	class={classes}
-	target={isHoldexLink ? '_self' : '_blank'}
-	rel="noreferrer"
+  title={item.title ? item.title : ''}
+  href={item.href}
+  class={classes}
+  target={isHoldexLink ? '_self' : '_blank'}
+  rel="noreferrer"
 >
-	<slot text={truncated} />
-	{#if !isHoldexLink}
-		<Icon icon={ArrowTopRightOnSquare} width={16} height={16} colorInherit />
-	{/if}
+  <slot text={truncated} />
+  {#if !isHoldexLink}
+    <Icon icon={ArrowTopRightOnSquare} width={16} height={16} colorInherit />
+  {/if}
 </a>
--- src/lib/components/Icons/types.ts
+++ src/lib/components/Icons/types.ts
@@ -1,8 +1,8 @@
 export interface SVGIconProps extends Omit<svelte.JSX.HTMLProps<HTMLElement>, 'size' | 'slot'> {
-	name: SVGIconName;
-	size?: string | number;
-	color?: string;
-	svgClass?: string;
+  name: SVGIconName;
+  size?: string | number;
+  color?: string;
+  svgClass?: string;
 }
 
 export type SVGIconName = 'sun' | 'moon' | 'logo';
