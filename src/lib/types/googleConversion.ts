bc764e5aef6cbb07c1da0ed6e025a8d03f85cb61
4kcancode <de4kcancode@outlook.com>
2023-09-05 19:47:57 +0900

    feat: refactor paragraph parser function

---
+++ src/lib/types/googleConversion.ts
@@ -0,0 +1,30 @@
+export type Parsed$ParagraphElement = {
+  [key: string]: string;
+};
+
+export type Parsed$ParagraphItems = {
+  content: string;
+  items: any[];
+};
+
+export type Parsed$Paragraph = {
+  type: 'nestedList' | 'embed' | string;
+  id?: string;
+  data: {
+    level?: number;
+    text?: string;
+    style?: string;
+    id?: string;
+    caption?: string;
+    alignment?: 'left' | 'right';
+    service?: 'twitter' | 'tally' | string;
+    url?: string;
+    source?: string;
+    embed?: string;
+    title?: string;
+    items?: Parsed$ParagraphItems[];
+    file?: {
+      url?: string;
+    };
+  };
+};
