a8397b4c4fb291d668415f7a5fd8888e8b5a751e
4kcancode <de4kcancode@outlook.com>
2023-12-11 15:48:02 +0800

    fix: implement instant gist color scheme switch
    Implement instant gist color scheme switch on theme toggle, using invert filter CSS

--- src/lib/components/BodyRenderer/Blocks/gist.svelte
+++ src/lib/components/BodyRenderer/Blocks/gist.svelte
@@ -17,38 +17,6 @@
   const barGistHeight = 30;
   onMount(() => {
     frame.srcdoc = `<script src='${item.embed}.js'><${''}/script>`;
-    frame.addEventListener('load', (ev) => {
-      const new_style_element = document.createElement('style');
-
-      if (localStorage.getItem('theme') === 'dark') {
-        new_style_element.textContent =
-          '.gist .gist-file { border: 1px solid #2F323D; }' +
-          '.gist .gist-data { background-color: #1C1F29 !important; border-bottom: 1px solid #2F323D; }' +
-          '.gist .gist-meta { background-color: #252933; color: #D7DAE0; }' +
-          '.gist .gist-meta a { color: #D7DAE0 }' +
-          '.gist .blob-num { background: #1C1F29; }' +
-          '.gist .blob-num:hover { color: #D7DAE0; }' +
-          '.gist .highlight { background: #1C1F29; }' +
-          '.gist .blob-code-inner { color: #FFF }' +
-          '.gist .pl-en { color: #fff; }' +
-          '.gist .pl-ent, .gist .pl-v { color: #fd0; }' +
-          '.gist .pl-mh, .gist .pl-mh .pl-en, .gist .pl-sr .pl-cce { color: #eb939a; }' +
-          '.gist .pl-pds, .gist .pl-s, .gist .pl-s1, .gist .pl-s1 .pl-pse .pl-s2, .gist .pl-s1 .pl-v { color: #3ad900; }' +
-          '.gist .pl-s1 .pl-s2, .gist .pl-smi, .gist .pl-smp, .gist .pl-stj, .gist .pl-vo, .gist .pl-vpf { color: #ccc; }' +
-          '.gist .pl-s3, .gist .pl-sc { color: #ffb054; }' +
-          '.gist .pl-sr, .gist .pl-sr .pl-sra, .gist .pl-sr .pl-sre, .gist .pl-src { color: #80ffc2; }' +
-          '.gist .pl-mdht, .gist .pl-mi1 { color: #f8f8f8; background: rgba(0, 64, 0, .5); }' +
-          '.gist .pl-id, .gist .pl-ii, .gist .pl-md, .gist .pl-mdhf { color: #f8f8f8; background: #800f00; }' +
-          '.gist .highlight-source-js .pl-st { color: #ffee80; }' +
-          '.gist .highlight-source-css .pl-s3 { color: #80ffbb; }' +
-          '.gist .highlight-text-html-basic .pl-ent { color: #9effff; }' +
-          '.gist .pl-c, .gist .pl-c span, .gist .pl-mq { color: #08f; }' +
-          '.gist .pl-c1, .gist .pl-sv, .gist .pl-mb { color: #ff628c; }' +
-          '.gist .pl-e, .gist .pl-k, .gist .pl-mdh, .gist .pl-mdr, .gist .pl-ml, .gist .pl-mm, .gist .pl-mo, .gist .pl-mp, .gist .pl-mr, .gist .pl-ms, .gist .pl-st, .gist .pl-mi { color: #ff9d00; }';
-      }
-
-      ev.target.contentDocument.head.appendChild(new_style_element);
-    });
   });
   function getInnerHeight() {
     height = frame?.contentWindow?.document?.body?.scrollHeight + barGistHeight + 'px';
@@ -56,7 +24,7 @@
 </script>
 
 <iframe
-  class="gist"
+  class="gist filter-none dark:invert"
   src="about:blank"
   bind:this={frame}
   {title}
@@ -73,6 +41,7 @@
     width: 710px;
     left: -53px;
   }
+
   @media screen and (min-width: 320px) and (max-width: 767px) {
     iframe.gist {
       position: relative;
