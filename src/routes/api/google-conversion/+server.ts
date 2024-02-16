fad1f74406ee03378bd4f6bdf7bd45d8addff833
4kcancode <de4kcancode@outlook.com>
2024-02-16 19:28:31 +0200

    fix: cta link  value parser

--- src/routes/api/google-conversion/+server.ts
+++ src/routes/api/google-conversion/+server.ts
@@ -314,7 +314,7 @@ function getImage(document: Schema$Document, element: Schema$ParagraphElement) {
   return null;
 }
 
-function getText(element: Schema$ParagraphElement, { isHeader = false } = {}) {
+function getText(element: Schema$ParagraphElement, { isHeader = false, isCtaLink = false } = {}) {
   let text = cleanText(element.textRun?.content as string);
   const { link, underline, strikethrough, bold, italic } = element?.textRun
     ?.textStyle as Schema$TextStyle;
@@ -338,6 +338,9 @@ function getText(element: Schema$ParagraphElement, { isHeader = false } = {}) {
   }
 
   if (link) {
+    if (isCtaLink) {
+      return (link.url || text) as string
+    }
     return `<a href="${link.url}">${text}</a>`;
   }
 
@@ -348,7 +351,8 @@ const parseParagraphElement = (
   document: Schema$Document,
   tag: string,
   parentContent: (Parsed$Paragraph | Parsed$ParagraphElement)[],
-  element: Schema$ParagraphElement
+  element: Schema$ParagraphElement,
+  wrappingTable: boolean | undefined = undefined
 ) => {
   const { textRun, richLink, inlineObjectElement } = element;
 
@@ -379,6 +383,7 @@ const parseParagraphElement = (
     parentContent.push({
       [tag]: getText(element, {
         isHeader: tag !== 'p',
+        isCtaLink: wrappingTable
       }),
     });
   }
@@ -557,7 +562,7 @@ const parseParagraph = (
           },
         });
       } else {
-        elements?.forEach((element) => parseParagraphElement(document, tag, tagContent, element));
+        elements?.forEach((element) => parseParagraphElement(document, tag, tagContent, element, wrappingTable));
       }
     }
 
